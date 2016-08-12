748ec5ec3f484de94eac32308537e2bcceb6a310
TaikiKosugi & Harrison Zandri & Lloyd Clave & Patrick <pair+TaikiKosugi.hzandri.llclave.patrickg112@devbootcamp.com>
2016-08-12 15:48:02 -0400

    enemies attack based on speed

--- app/assets/javascripts/character.js
+++ app/assets/javascripts/character.js
@@ -3,4 +3,10 @@ var Character = function Character(){
   this.pos_x = 0;
   this.pos_y = 0;
   this.speed = 4;
+
+  this.health = 100;
+  this.max_health = 100;
+  this.attack = 10;
+  this.xp = 0;
+  this.defense = 1;
 };
