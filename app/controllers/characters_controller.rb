341e902c2adc5c0aec9e4d2c2c7d47fd2d3d655c
patrickg112 <patrickg112@gmail.com>
2016-08-13 18:46:18 -0400

    still working

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,14 +1,13 @@
 class CharactersController < ApplicationController
-  before_action :check_user, :set_character_name
   before_action :check_character, only: :game
-
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def new
-    @character = Character.new
+    @character = current_user.characters.build
   end
 
   def create
-    @character = Character.new(params.require(:character).permit(:name))
-    @character.user_id = session[:user_id]
+    @character = current_user.characters.build(character_params)
     if @character.save
       flash[:notice] = "\"#{@character.name}\" has been successfully created."
       session[:character_id] = @character.id
@@ -38,12 +37,17 @@ class CharactersController < ApplicationController
       end
     end
 
-    def set_character_name
-      character = Character.find_by(id: session[:character_id])
-      @character_name = nil
-      if character
-        @character_name = character.name
-      end
-    end
+    # def set_character_name
+    #   character = Character.find_by(id: session[:character_id])
+    #   @character_name = nil
+    #   if character
+    #     @character_name = character.name
+    #   end
+    # end
+      private
 
+  def character_params
+    params.require(:character).permit(:name, :location, :user_id, :party_id, :attack, :health, :XP, :defense, :max_health)
+  end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
-  has_many :characters
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, length: {minimum: 5}, uniqueness: true
-  validates :email, uniqueness: true
+   has_many :characters
 end
